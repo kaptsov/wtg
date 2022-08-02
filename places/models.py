9efae345becf999f3e7ad478ffe5ddb2e0ae0a33
alexanderkaptsov <kaptsovaa@gmail.com>
2022-08-02 20:45:19 +0400

    Cleaned code (pep8)

--- places/models.py
+++ places/models.py
@@ -7,13 +7,14 @@ class Place(models.Model):
     title = models.TextField(max_length=200,
                              help_text='Не более 200 символов',
                              verbose_name='Название',
-                             blank=True)
+                             blank=True
+                             )
     description_short = models.TextField(verbose_name='Короткое описание',
                                          blank=True
                                          )
     description_long = HTMLField(verbose_name='Полное описание',
-                                        blank=True
-                                        )
+                                 blank=True
+                                 )
     lat = models.FloatField(verbose_name='Положение по широте')
     lng = models.FloatField(verbose_name='Положение по долготе')
 
@@ -28,11 +29,9 @@ class Image(models.Model):
                               related_name='images',
                               verbose_name='Место')
     index = models.PositiveIntegerField(verbose_name='Порядковый номер',
-                                             default=0,
-                                             blank=False,
-                                             null=False,
-                                             db_index=True
-                                             )
+                                        default=0,
+                                        db_index=True
+                                        )
     img_file = models.ImageField(verbose_name='Изображение')
 
     class Meta:
