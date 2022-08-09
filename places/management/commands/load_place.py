5ea08c5fcf52585378fec33e16b10a94081a4be7
kaptsov <kaptsovaa@gmail.com>
2022-08-09 10:38:11 +0400

    refactored in flat slyle

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -9,7 +9,7 @@ from places.models import Place, Image
 
 class Command(BaseCommand):
     """
-    command: python manage.py load_place <link_to_file>
+    command: python manage.py load_place <link_to_json_file>
     Команда для заполнения данными локации на карте.
     Принимает аргументом ссылку на json-файл с описанием локации или файл со списком таких ссылок.
     Если объекта нет в базе данных, он будет создан.
@@ -23,7 +23,10 @@ class Command(BaseCommand):
 
     def fill_data_from_link(self, link):
         response = requests.get(link.strip())
-        if response.ok:
+        if not response.ok:
+            self.stdout.write(self.style.ERROR(f'Server returned an error: {response.status_code}'))
+            response.raise_for_status()
+        else:
             location_data = response.json()
             if 'error' in location_data:
                 raise requests.exceptions.HTTPError(location_data['error'])
@@ -36,23 +39,20 @@ class Command(BaseCommand):
                     'description_short': location_data['description_short'],
                 }
             )
-            if created:
-                for img in location_data['imgs']:
-                    img_file = ContentFile(requests.get(img).content)
-                    filename = img.split('/')[-1]
-                    loc_img, created = Image.objects.get_or_create(
-                        place_id=location.id,
-                        img_file=filename
-                    )
-                    if created:
-                        loc_img.img_file.save(filename, img_file, save=True)
-                self.stdout.write(self.style.SUCCESS(f'Successfully read file {link}'
-                                                     f'Created Location object: {location.title} \n'))
-            else:
-                self.stdout.write(self.style.WARNING(f'Location {location.title} already exists, defaults updated.'))
+        if not created:
+            self.stdout.write(self.style.WARNING(f'Location {location.title} already exists, defaults updated.'))
         else:
-            self.stdout.write(self.style.ERROR(f'Server returned an error: {response.status_code}'))
-            response.raise_for_status()
+            for img in location_data['imgs']:
+                img_file = ContentFile(requests.get(img).content)
+                filename = img.split('/')[-1]
+                loc_img, created = Image.objects.get_or_create(
+                    place_id=location.id,
+                    img_file=filename
+                )
+                if created:
+                    loc_img.img_file.save(filename, img_file, save=True)
+            self.stdout.write(self.style.SUCCESS(f'Successfully read file {link}'
+                                                 f'Created Location object: {location.title} \n'))
 
     def handle(self, *args, **kwargs):
         source = str(kwargs['source'])
