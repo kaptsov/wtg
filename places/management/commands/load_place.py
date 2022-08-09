261c0f60f7c337473d51076c6a4a8c4e60c9cd6d
kaptsov <kaptsovaa@gmail.com>
2022-08-09 10:59:06 +0400

    splitted main function

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -7,6 +7,31 @@ from django.core.management.base import BaseCommand
 from places.models import Place, Image
 
 
+def get_create_location(location_data):
+    location, created = Place.objects.get_or_create(
+        title=location_data['title'],
+        defaults={
+            'lng': location_data['coordinates']['lng'],
+            'lat': location_data['coordinates']['lat'],
+            'description_long': location_data['description_long'],
+            'description_short': location_data['description_short'],
+        }
+    )
+    return location, created
+
+
+def upload_pics(location_data, location):
+    for img in location_data['imgs']:
+        img_file = ContentFile(requests.get(img).content)
+    filename = img.split('/')[-1]
+    loc_img, created = Image.objects.get_or_create(
+        place_id=location.id,
+        img_file=filename
+    )
+    if created:
+        loc_img.img_file.save(filename, img_file, save=True)
+
+
 class Command(BaseCommand):
     """
     command: python manage.py load_place <link_to_json_file>
@@ -29,30 +54,16 @@ class Command(BaseCommand):
         if 'error' in location_data:
             raise requests.exceptions.HTTPError(location_data['error'])
 
-        location, created = Place.objects.get_or_create(
-            title=location_data['title'],
-            defaults={
-                'lng': location_data['coordinates']['lng'],
-                'lat': location_data['coordinates']['lat'],
-                'description_long': location_data['description_long'],
-                'description_short': location_data['description_short'],
-            }
-        )
+        location, created = get_create_location(location_data)
 
         if not created:
             self.stdout.write(self.style.WARNING(f'Location {location.title} already exists, defaults updated.'))
-        else:
-            for img in location_data['imgs']:
-                img_file = ContentFile(requests.get(img).content)
-                filename = img.split('/')[-1]
-                loc_img, created = Image.objects.get_or_create(
-                    place_id=location.id,
-                    img_file=filename
-                )
-                if created:
-                    loc_img.img_file.save(filename, img_file, save=True)
-            self.stdout.write(self.style.SUCCESS(f'Successfully read file {link}'
-                                                 f'Created Location object: {location.title} \n'))
+            return
+
+        upload_pics(location_data, location)
+
+        self.stdout.write(self.style.SUCCESS(f'Successfully read file {link}'
+                                             f'Created Location object: {location.title} \n'))
 
     def handle(self, *args, **kwargs):
         source = str(kwargs['source'])
